80d15e58e734e9d33c04df74ca0f2d1c524adf97
Xinyi Zhang <henry@Maomao-no1.local>
2022-05-30 12:06:38 -0700

    xz - pulling from master

--- solver.py
+++ solver.py
@@ -38,15 +38,29 @@ def sanity_check(config_info: dict) -> None:
             if seat_constraint > 2 or seat_constraint < 0:
                 raise IOError(f"{seat_constraint} isn't a valid seat constraint for {name}. Constraint must be specified as 0, 1, or 2.")
 
+def configure_solver(config_info: dict, cp_solver: Solver) -> None:
+    """
+    Encodes the constraints from input file as an SMT formula
+    config_info: configuration information
+    cp_solver: z3 Solver object to construct with SMT formula
+    rtype: None
+    """
+    # generate position variables: X_p_c_s (person p in car number c in seat type s)
+    X = [[[]]]
+    # encode constraint: one person in one type of seat on one car
+    cp_solver.add(x + 1 == 4)
+
 def main() -> None:
     # gather user input
     input.main()
-
     # sanity checks
     sanity_check(input.config_info)
     # convert specifications to a series of z3 constraints
-
+    carpool_solver = Solver()
+    configure_solver(input.config_info, carpool_solver)
     # run the solver
+    print(carpool_solver.check())
+    print(carpool_solver.model())
 
     # display solver output
 
